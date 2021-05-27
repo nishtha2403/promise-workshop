35c6e1d32f514f114958dd9b9475bf31aed7ab18
nishtha <nishtha.24sharma1998@gmail.com>
2021-05-27 11:39:48 +0530

    10th problem (2nd way)
    handle the multiple array in another way

--- multiplePromises.js
+++ multiplePromises.js
@@ -1,17 +1,25 @@
 function all(promise1,promise2) {
     let resolvedArray = [];
+    let counter = 0;
 
-    return new Promise((resParent) => {
-        promise1.then((resolvedValue) => {
-            resolvedArray.push(resolvedValue);
-            
-            promise2.then((res) => {
-                resolvedArray.push(res);
-                
-                resParent(resolvedArray);
-            });
+    return new Promise((resolve) => {
+        promise1.then((res) => {
+            resolvedArray[0] = res;
+            counter += 1;
+            if(counter == 2){
+                resolve(resolvedArray);
+            }
         });
-    });
+
+        promise2.then((res) => {
+            resolvedArray[1] = res;
+            counter += 1;
+
+            if(counter == 2){
+                resolve(resolvedArray);
+            }
+        })
+    })
 
 }
 
